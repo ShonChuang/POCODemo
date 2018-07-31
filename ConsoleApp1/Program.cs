5386ac39ce23f95ed0b2bfdec4f4b7bac1408f51
Shon <hsuizip@gmail.com>
2018-07-31 15:27:37 +0800

    EF資料存取

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var db = new MyDbContext();
+            var data = db.SalesLT_Products
+                .Where(d => d.Size != null).Take(10).ToList();
+
+            var data2 = (from product in db.SalesLT_Products
+                        where product.Size != null
+                        select product)
+                        .Take(10);
+                       
+                   
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.ProductId},{item.Name}");
+            }
+            Console.WriteLine("==========================================");
+            foreach (var item in data2)
+            {
+                Console.WriteLine($"{item.ProductId},{item.Name}");
+            }
+            // Console.ReadKey();
         }
     }
 }
